a1488a3727ebfa319dde05c4c3c4df67d25b7476
Matthieu Desplantes <matmututu@gmail.com>
2020-11-25 11:01:33 +0100

    UPDATE: trying to save a translation when nothing changed

--- traduko/ajax.py
+++ traduko/ajax.py
@@ -4,60 +4,64 @@ from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 from .models import *
-from datetime import datetime
 from .translation_functions import *
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 import json
 
 
 @csrf_exempt
 @login_required
 def save_translation(request, trstring_id, language):
-    # TODO: if no change
     if request.method != "POST":
         return HttpResponse('')
 
     current_language = get_object_or_404(Language, code=language)
     current_string = get_object_or_404(TrString, pk=trstring_id)
     editmode = current_language == current_string.project.source_language
+    new_pluralized = bool(request.POST.get('pluralized') == 'true')
 
     if not is_allowed_to_translate(request.user, current_string, current_language):
         return HttpResponse('Vi ne rajtas traduki al tiu lingvo (' + current_language.name + ').')
 
     try:
         translated_text = TrStringText.objects.get(language=current_language, trstring=current_string)
+        new_translation = False
     except ObjectDoesNotExist:
         translated_text = TrStringText(language=current_language, trstring=current_string)
+        new_translation = True
 
     parsed_text_data = parse_submitted_text(request.POST.get('text'),
                                             TrStringText.objects.get(trstring=current_string, language=current_string.project.source_language).pluralized,
                                             current_language.nplurals())
 
-    translated_text.state = TRANSLATION_STATE_TRANSLATED
-    translated_text.translated_by = request.user
-    translated_text.text = parsed_text_data['text']
+    if new_translation or parsed_text_data['text'] != translated_text.text or (editmode and translated_text.pluralized != new_pluralized):  # If there are changes
+        translated_text.state = TRANSLATION_STATE_TRANSLATED
+        translated_text.translated_by = request.user
+        translated_text.text = parsed_text_data['text']
 
-    if editmode:  # Update word and character count
-        translated_text.pluralized = bool(request.POST.get('pluralized') == 'true')
-        current_string.context = request.POST.get('context')
-        current_string.words = parsed_text_data['words']
-        current_string.characters = parsed_text_data['characters']
-        current_string.save()
-        update_string_count(current_string.project)
+        if editmode:  # Update word and character count
+            translated_text.pluralized = new_pluralized
+            current_string.context = request.POST.get('context')
+            current_string.words = parsed_text_data['words']
+            current_string.characters = parsed_text_data['characters']
+            current_string.save()
+            update_string_count(current_string.project)
 
-        minor_change = bool(request.POST.get('minor') == 'true')  # Update translation status of translations
-        if not minor_change:
-            current_string.trstringtext_set.exclude(language=current_language).update(state=TRANSLATION_STATE_OUTDATED)
-            translated_text.last_change = datetime.now()
-    else:
-        original_text = get_object_or_404(TrStringText, trstring=current_string,
-                                          language=current_string.project.source_language)
-        translated_text.pluralized = original_text.pluralized
-        translated_text.last_change = datetime.now()
+            minor_change = bool(request.POST.get('minor') == 'true')  # Update translation status of translations
+            if not minor_change:
+                current_string.trstringtext_set.exclude(language=current_language).update(state=TRANSLATION_STATE_OUTDATED)
+                translated_text.last_change = timezone.now()
+        else:
+            original_text = get_object_or_404(TrStringText, trstring=current_string,
+                                              language=current_string.project.source_language)
+            translated_text.pluralized = original_text.pluralized
+            translated_text.last_change = timezone.now()
 
-    translated_text.save()
+        translated_text.save()
 
-    current_string.state = TRANSLATION_STATE_TRANSLATED
+    # Adding parameters for template
+    current_string.state = translated_text.state
     current_string.translated_text = translated_text
     current_string.original_text = TrStringText.objects.get(language=current_string.project.source_language, trstring=current_string)
 
