5a189f07d0a5761466e3619fb5130f69da880540
Matthieu Desplantes <matmututu@gmail.com>
2021-06-02 15:20:13 +0200

    updated requirements and Django to 3.2.4

--- traduko/apps.py
+++ traduko/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class TradukoConfig(AppConfig):
     name = 'traduko'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         import traduko.signals
