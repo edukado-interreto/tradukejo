a4db9c3fd1f327c637297263c1476bf2ca17543a
Matthieu Desplantes <matmututu@gmail.com>
2021-10-22 10:42:46 +0200

    missing JS files

---
+++ vue-translation-interface/src/lang/it.js
@@ -0,0 +1,60 @@
+export default {
+  "choose": "Seleziona la lingua nella quale vuoi tradurre",
+  "delete": "Cancella",
+  "load_more": "Carica di più",
+  "comments": {
+    "add": "Aggiungi commento",
+    "comments": "Commenti",
+    "delete_confirm": "Sicuri di voler cancellare questo commento?",
+    "link": "Commenta",
+    "number": "{n} commento | {n} commenti",
+    "write": "Scrivi il tuo commento qui..."
+  },
+  "filters": {
+    "all": "Tutte le stringhe",
+    "order_name": "Ordina per nome",
+    "order_new": "Ordina per data (nuovo per primo)",
+    "order_old": "Ordina per data (vecchio per primo)",
+    "outdated": "Solo da aggiornare",
+    "search": "Cerca",
+    "untranslated": "Solo da tradurre",
+    "untranslated_outdated": "Da tradurre e da aggiornare"
+  },
+  "history": {
+    "current": "(versione corrente)",
+    "hide_deleted": "Nascondi le parti cancellate",
+    "show_deleted": "Mostra le parti cancellate",
+    "versions": "Cronologia ({n} versione) | Cronologia ({n} versioni)"
+  },
+  "navigation": {
+    "all": "Tutte",
+    "characters": "Caratteri",
+    "count": "Stringhe: {strings}, parole: {words}",
+    "count_children": "Stringhe: {strings} ({strings2} in sottocartelle), parole: {words} ({words2} in sottocartelle)",
+    "strings": "Stringhe",
+    "words": "Parole"
+  },
+  "translate": {
+    "add_string": "Aggiungi stringa",
+    "auto_translate": "Traduci con {name}",
+    "cancel": "Annulla",
+    "context": "Spiegazione del contenuto della stringa",
+    "deleted": "La stringa {name} è stata cancellata.",
+    "edit": "Modifica",
+    "link": "Collegamento diretto a questa stringa",
+    "mark_outdated": "Segna come da aggiornare",
+    "mark_translated": "Segna come tradotta",
+    "no_result": "Nessun risultato. Prova a usare gli altri filtri.",
+    "number": "Se il numero è {n}:",
+    "number_explanations": "Questa stringa ha diverse forme a seconda del numero.",
+    "outdated": "Da aggiornare",
+    "save": "Salva",
+    "special_symbols": "La traduzione non contiene i seguenti singoli speciali: {s}. Sicuri di voler salvare la traduzione?",
+    "string_name": "Nome della stringa",
+    "string_path": "Percorso della stringa",
+    "suggestions": "Traduzioni suggerite",
+    "translate": "Traduci",
+    "translated": "Tradotta",
+    "untranslated": "Non tradotta"
+  }
+}
